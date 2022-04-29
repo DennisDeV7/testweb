4c1bcf7abf51406f4941e6d384f53be2c8daaa8e
Dennis DeVries <7dennisdv7@gmail.com>
2022-04-28 21:34:30 -0400

    add image prompt and loop for lab 8

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 'use strict';
 
-var username = prompt("What is your name?");
-
-console.log(username);
-
-if (username === "Dennis") {
-    console.log("Hey Dennis!");
-} else {
-    console.log("Welcome " + username);
+function greeting () {
+    let username = prompt("What is your name?");
+    console.log(username);
+    if (username === "Dennis") {
+        console.log("Hey Dennis!");
+    } else {
+        console.log("Welcome " + username);
+    }
+    return username
 }
 
-document.write(username);
-
-let response = prompt("What is Dennis's favorite color?");
-
-response.toLowerCase();
-
-if (response === "blue") {
-    document.write("You are correct!");
-} else if (response == "rainbow") {
-    document.write("Only if there is gold");
-} else {
-    document.write("Wrong!!");
+function guesscolor () {  
+    let response;     
+    while (response !== "blue") {
+        response = prompt("What is Dennis's favorite color?").toLowerCase();
+        console.log(response);  
+        if (response == "blue") {
+            return response
+        }      
+    } 
 }
+
+function picGenerator () {
+    let number = prompt("How many pictures would you like to see? (1-5)");
+    console.log(number);
+    for (let i=0; i < number; i++){
+        // const img = document.createElement("img");
+        // img.src="https://via.placeholder.com/75";
+        document.write("<img src='https://via.placeholder.com/75'class='pictures'/>");
+    }
+}
+
+let userName = greeting();
+document.write(userName);
+
+let userguess = guesscolor();
+document.write("Correct!", userguess);
+
+// picGenerator()
+
+
+
+
+
+// if (response === "blue") {
+//     document.write("You are correct!");
+// } else if (response == "rainbow") {
+//     document.write("Only if there is gold");
+// } else {
+//     document.write("Wrong!! try again");
+//     let response = prompt("What is Dennis's favorite color?");
+//     if (response === "blue") {
+//         document.write("You are correct!")
+//     } else {
+//         document.write("Try again some other time.")
+//     }
+// } 
+
